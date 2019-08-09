3748d2a0cdbd611528a56272aa30ce203472e6eb
yellow013 <wk9988@gmail.com>
2019-08-09 23:57:53 +0800

    update gui

--- src/main/java/io/ffreedom/corporal/RedStoneGuiLaunch.java
+++ src/main/java/io/ffreedom/corporal/RedStoneGuiLaunch.java
@@ -9,31 +9,22 @@ import javax.swing.JPanel;
 import javax.swing.JTable;
 import java.awt.FlowLayout;
 
-public class NewGui0 {
+public class RedStoneGuiLaunch {
 
-	private JFrame mainWingdowd;
+	private JFrame mainWindow;
 	private JTable dataTable;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					NewGui0 window = new NewGui0();
-					window.mainWingdowd.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		EventQueue.invokeLater(() -> new RedStoneGuiLaunch().mainWindow.setVisible(true));
 	}
 
 	/**
 	 * Create the application.
 	 */
-	public NewGui0() {
+	public RedStoneGuiLaunch() {
 		initialize();
 	}
 
@@ -41,28 +32,28 @@ public class NewGui0 {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		mainWingdowd = new JFrame();
-		mainWingdowd.setBounds(100, 100, 450, 300);
-		mainWingdowd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainWingdowd.getContentPane().setLayout(new BorderLayout(0, 0));
-		
+		mainWindow = new JFrame();
+		mainWindow.setBounds(100, 100, 450, 300);
+		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainWindow.getContentPane().setLayout(new BorderLayout(0, 0));
+
 		JPanel topPanel = new JPanel();
-		FlowLayout fl_topPanel = (FlowLayout) topPanel.getLayout();
-		fl_topPanel.setAlignment(FlowLayout.LEFT);
-		mainWingdowd.getContentPane().add(topPanel, BorderLayout.NORTH);
-		
+		FlowLayout topPanelFlowLayout = (FlowLayout) topPanel.getLayout();
+		topPanelFlowLayout.setAlignment(FlowLayout.LEFT);
+		mainWindow.getContentPane().add(topPanel, BorderLayout.NORTH);
+
 		JButton strategyButton = new JButton("策略");
 		topPanel.add(strategyButton);
-		
+
 		JButton orderButton = new JButton("订单");
 		topPanel.add(orderButton);
-		
+
 		JButton posButton = new JButton("持仓");
 		topPanel.add(posButton);
-		
+
 		JPanel centerPanel = new JPanel();
-		mainWingdowd.getContentPane().add(centerPanel, BorderLayout.CENTER);
-		
+		mainWindow.getContentPane().add(centerPanel, BorderLayout.CENTER);
+
 		dataTable = new JTable();
 		centerPanel.add(dataTable);
 	}
